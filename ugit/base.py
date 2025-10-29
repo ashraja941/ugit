79c15c2b88380d41e3d0107d933a8319a2b4979d
ash <ashraja941@gmail.com>
2025-10-29 18:08:46 -0500

    feat: write-tree function

--- ugit/base.py
+++ ugit/base.py
@@ -1,1 +1,35 @@
+import os
+
 from . import data
+
+
+def write_tree(directory: str = ".") -> str:
+    entries: list[tuple[str, str, str]] = []
+    with os.scandir(directory) as it:
+        for entry in it:
+            full_file_location: str = os.path.join(directory, entry.name)
+            if is_ignored(full_file_location):
+                continue
+
+            type_: str = ""
+            if entry.is_file(follow_symlinks=True):
+                type_ = "blob"
+                with open(full_file_location, "rb") as f:
+                    oid = data.hash_object(f.read(), type_)
+
+            elif entry.is_dir(follow_symlinks=True):
+                type_ = "tree"
+                oid = write_tree(full_file_location)
+            else:
+                raise NotImplementedError
+            entries.append((entry.name, oid, type_))
+
+    tree = "".join(f"{type_} {oid} {name}\n" for name, oid, type_ in sorted(entries))
+    return data.hash_object(tree.encode(), "tree")
+
+
+def is_ignored(path: str) -> bool:
+    # multiple slashes to account for different os
+    for delimiter in ["/", "\\"]:
+        path = path.replace(delimiter, " ")
+    return ".ugit" in path.split(" ")
--- ugit/cli.py
+++ ugit/cli.py
@@ -28,6 +28,9 @@ def parse_args():
     cat_file_parser.set_defaults(func=cat_file)
     cat_file_parser.add_argument("object")
 
+    init_parser = commands.add_parser("write-tree")
+    init_parser.set_defaults(func=write_tree)
+
     return parser.parse_args()
 
 
@@ -60,3 +63,7 @@ def cat_file(args) -> None:
     """
     _ = sys.stdout.flush()
     sys.stdout.buffer.write(data.get_object(args.object, expected=None))
+
+
+def write_tree(args) -> None:
+    print(base.write_tree())
