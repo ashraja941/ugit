38ed23f9551c63651ce64147d2f571a5ad2846d9
ash <ashraja941@gmail.com>
2025-11-13 12:53:33 -0600

    feat: diff between files

--- ugit/diff.py
+++ ugit/diff.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+import difflib
+from . import data
 
 
 def compare_trees(*trees: dict[str, str]):
@@ -22,6 +24,27 @@ def diff_trees(t_from: dict[str, str], t_to: dict[str, str]):
     """
     output = ""
     for path, o_from, o_to in compare_trees(t_from, t_to):
-        if o_from != o_to:
-            output += f"changed: {path}\n"
+        _ = path
+        if o_from != o_to and o_from is not None and o_to is not None:
+            # output += f"changed: {path}\n"
+            output += diff_blobs(o_from, o_to)
     return output
+
+
+def diff_blobs(t_from: str, t_to: str) -> str:
+    """
+    Display the difference between 2 files
+
+    Args: OIDs of files
+    Returns: str output
+    """
+    data_from = data.get_object(t_from).decode()
+    data_to = data.get_object(t_to).decode()
+
+    lines1 = data_from.splitlines()
+    lines2 = data_to.splitlines()
+
+    diff = difflib.unified_diff(
+        lines1, lines2, fromfile="Previous", tofile="New", lineterm=""
+    )
+    return "\n".join(diff)
